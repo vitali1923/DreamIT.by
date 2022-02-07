781ce66fc23d0685068fb3cdb97caa9070ce701b
vitali1923 <ooomevir@gmail.com>
2022-02-07 22:48:18 +0300

    ДОБАВИЛ send.php

---
+++ send.php
@@ -0,0 +1,58 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$massage = $_POST['massage'];
+
+// Формирование самого письма
+$title = "Новое обращение mf.dreamit";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Почта:</b> $email<br><br>
+<b>Сообщение:</b><br>$massage
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'hummel.sport@mail.ru'; // Логин на почте
+    $mail->Password   = 'iNE9nsgfoK6TBwMDtB94'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('hummel.sport@mail.ru', 'Vitali Melnik'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('vitalimelnik@yandex.ru'); 
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
