7e008d2cdc99db1e9d4d77e378c0cc26b5f563a7
vitali1923 <ooomevir@gmail.com>
2022-02-07 09:08:03 +0300

    Добавил мобильное меню, подключил main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var menuButton = document.querySelector('.navbar-button');
+var menuButton = document.querySelector ('.menu-button');
 menuButton.addEventListener('click',function () {
 console.log("Клик по кнопке меню");
+document.querySelector(".navbar-button").classList.toggle('navbar-button--visible');
 });
