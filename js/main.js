56438207c88ebb0d8e2f36c0c66b03e6ae859aff
vitali1923 <ooomevir@gmail.com>
2022-02-08 10:50:13 +0300

    Подключил jquery

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ menuButton.addEventListener('click',function () {
 console.log("Клик по кнопке меню");
 document.querySelector(".navbar-button").classList.toggle('navbar-button--visible');
 });
+
+// Обработка формы
+$(".form").validate({
+    messages: {
+        name: {
+        required: "Введите Ваше имя",
+        minlength: jQuery.validator.format("Требуется не менее 2 символов")
+        },
+        phone: {
+            required: "Введите Ваш номер телефона",
+            minlength: jQuery.validator.format("Требуется не менее 9 символов")
+        },
+        email:"Ваш адрес должен быть в формате name@domain.com"
+    },
+});
+$("#phone").mask("+375(99)999-99-99");
